8b58350456a601f233eef49a97edafacb24aca83
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-05-05 20:59:14 +0500

    Q5 done

--- script.js
+++ script.js
@@ -26,3 +26,34 @@
 //     console.log(`row deleted`)
  
 // }
+
+// <!-- ===============Q4=============== -->
+
+// 4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
+// function imgOver(){
+//    document.getElementById("img1").src = "https://images.pexels.com/photos/24024088/pexels-photo-24024088/free-photo-of-elegant-facade-of-store.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+// }
+
+// function imgOut(){
+//     document.getElementById("img1").src = "https://images.pexels.com/photos/31477151/pexels-photo-31477151/free-photo-of-blossoming-cherry-blossom-tree-in-seoul-s-historic-palace.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+// }
+
+
+// 5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.
+
+let count = 0
+let result = document.getElementById("count")
+
+function add(){
+   count++
+   result.textContent = count
+}
+
+function subract(){
+   count--
+   result.textContent = count
+}
