a3808c3718392734608fc378c9cbf77375a85e6b
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-05-05 13:57:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. Show an alert box on click on a link.
+
+// function showAlert(event){
+//     event.preventDefault()
+//     alert("You click the link!")
+//     console.log("clicked")
+// }
+
+
+// 2. Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+
+// function showAlert(event) {
+//     event.preventDefault()
+//     alert("Thank you for purchasing a phone from us")
+//     console.log("clicked")
+// }
+
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted. 
+
+
+// function deleteRow(btn){
+//     btn.parentElement.parentElement.remove();
+//     console.log(`row deleted`)
+ 
+// }
